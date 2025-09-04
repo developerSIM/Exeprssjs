a4357a73350cc26f7c5680a8494d3687bdfb75ec
tlarjsqh <geonbosim318@gmail.com>
2025-09-04 20:19:47 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+
+const menus = [
+  { name: "아메리카노", price: 1500, kcal: 0 },
+  { name: "라떼", price: 2500, kcal: 150 },
+  { name: "바닐라", price: 3000, kcal: 200 },
+];
+
+
+const positionType = ["supervisor", "manager", "parttime"];
+
+const staffs = [
+  { name: "김자바", age: 22, position: "parttime" },
+  { name: "이파썬", age: 25, position: "manager" },
+  { name: "박자스", age: 23, position: "parttime" },
+];
+
+module.exports = { menus, staffs, positionType };
